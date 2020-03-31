db44315e90731875b163483762bbafab56881ad9
ausaafnabi <nabiausaaf@gmail.com>
2020-03-31 14:05:32 +0530

    Adding basic file Schema

--- Tests/test_CLI.py
+++ Tests/test_CLI.py
@@ -2,6 +2,6 @@ import unittest
 import importlib
 import CLI
 
-class FirstTest(unittest.TestCase):
-    def test(self):
-        self.assertEqual()
+#class FirstTest(unittest.TestCase):
+ #   def test(self):
+  #      self.assertEqual()
