8344d245b6e8e59de7af798c3d89abd89e78702c
ausaafnabi <nabiausaaf@gmail.com>
2020-04-04 16:37:50 +0530

    Edited Phase_template.py

---
+++ core/CodeCable/Phase_template.py
@@ -0,0 +1,11 @@
+Initializer = {
+    "Usage" : "To initialize the data",
+     "code" : "lorem ipsum dolor set emit constrecteur",
+     "discription" : "some discription"
+}
+ 
+CODEBASE = {
+  "Pipelines" : {
+    "Initializer" : Initializer
+  }
+} 
